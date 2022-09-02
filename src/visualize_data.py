3a08d7d8a29744a172fa7780cfcd941af83b2e7b
T-S1 <yama153shou@gmail.com>
2022-09-02 21:19:14 +0900

    fixed bug

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -6,11 +6,6 @@ plt.rcParams['font.family'] = "Meiryo"  # フォントの設定
 
 
 def show_image(image):
-    """show a image
-
-    Args:
-        image (numpy.ndarray): shape is (height, width, channels)
-    """
     if np.amax(image) > 1:
         im = image.astype(np.int32)
     else:
@@ -22,31 +17,21 @@ def show_image(image):
 
 
 def show_images(images, nrows=4, ncols=5):
-    """show images in a figure
-    Args:
-        images (numpy.ndarray): shape is (num_data, height, width, channels)
-    """
     if np.amax(images) > 1:
         ims = images.astype(np.int32)
     else:
         ims = images.astype(np.float64)
     n_plots = min(len(ims), nrows * ncols)
-    fig, axs = plt.subplots(nrows, ncols)
+    fig = plt.figure()
     for i in range(n_plots):
-        row = i // ncols
-        col = i % ncols
-        axs[row, col].axis("off")
-        axs[row, col].imshow(ims[i])
+        ax = fig.add_subplot(nrows, ncols, i + 1)
+        ax.axis("off")
+        ax.imshow(ims[i])
     plt.tight_layout()
     plt.show()
 
 
 def show_similarities(sims, threshold=None):
-    """show similarities for each data
-    Args:
-        sims (numpy.ndarray): shape is (num_data)
-        threshold (float): threshold for anomaly detection
-    """
     plt.xlabel("データ番号")
     plt.ylabel("類似度")
     plt.plot(sims, "o")
@@ -57,27 +42,21 @@ def show_similarities(sims, threshold=None):
 
 
 def show_results(images, labels, values=None, nrows=4, ncols=5, names=["正常", "異常"], value_name="評価値"):
-    """show images in a figure
-    Args:
-        images (numpy.ndarray [int]): shape is (num_data, height, width, channels)
-        labels (list [str]): list of normal or anomaly
-    """
     if np.amax(images) > 1:
         ims = images.astype(np.int64)
     else:
         ims = images
     n_plots = min(len(ims), nrows * ncols)
-    fig, axs = plt.subplots(nrows, ncols)
+    fig = plt.figure()
     for i in range(n_plots):
-        row = i // ncols
-        col = i % ncols
         pred = int(labels[i])
         title = f"予測: {names[pred]}"
         if values is not None:
             title += f", {value_name}: {values[i]:.4f}"
-        axs[row, col].set_title(title)
-        axs[row, col].axis("off")
-        axs[row, col].imshow(ims[i])
+        ax = fig.add_subplot(nrows, ncols, i + 1)
+        ax.set_title(title)
+        ax.axis("off")
+        ax.imshow(ims[i])
     plt.tight_layout()
     plt.show()
 
@@ -90,14 +69,14 @@ def show_history(history):
     ax1.set_title("Model accuracy")
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Accuracy")
-    ax1.legend(["Train", "Validation"], loc="upper left")
+    ax1.legend(["Train", "Validation"], loc="lower right")
 
     ax2.plot(history.history["loss"])
     ax2.plot(history.history["val_loss"])
     ax2.set_title("Model accuracy")
     ax2.set_xlabel("Epoch")
     ax2.set_ylabel("Loss")
-    ax2.legend(["Train", "Validation"], loc="upper left")
+    ax2.legend(["Train", "Validation"], loc="upper right")
 
     plt.tight_layout()
     plt.show()
